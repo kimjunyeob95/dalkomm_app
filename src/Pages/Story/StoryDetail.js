e20a7f9082472be5901f5f29af050ab249a438a1
junyeob kim <kimjun@hanbom.com>
2021-11-08 19:18:34 +0900

    작업

--- src/Pages/Story/StoryDetail.js
+++ src/Pages/Story/StoryDetail.js
@@ -14,7 +14,6 @@ import { moveScrollTop } from "Jquery/Jquery";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination } from "swiper/core";
 import { fadeOut } from "Config/GlobalJs";
-import { authContext } from "ContextApi/Context";
 
 export default function StoryDetail() {
   const history = useHistory();
@@ -78,129 +77,152 @@ export default function StoryDetail() {
   const handleLocation = (seq) => {
     history.push(`/story/detail/${seq}`);
   };
-  return (
-    <React.Fragment>
-      <GoContents />
+  if (axioData) {
+    return (
+      <React.Fragment>
+        <GoContents />
 
-      <div id="wrap" className="wrap">
-        <div id="container" className="container">
-          {/* <HeaderSub title="달콤스토리" btnType="share" icon="share" blindClass={false} PathName="detail" /> */}
-          <header id="header" className="header bg-transparent">
-            <h1 className="page-title">
-              <span className="blind">달콤스토리</span>
-            </h1>
-            <Link type="button" className="btn back" to={"/story/list"}>
-              <i className="ico back">
-                <span className="blind">뒤로</span>
+        <div id="wrap" className="wrap">
+          <div id="container" className="container">
+            <header id="header" className="header bg-transparent">
+              <h1 className="page-title">
+                <span className="blind">달콤스토리</span>
+              </h1>
+              <Link type="button" className="btn back" to={"/story/list"}>
+                <i className="ico back">
+                  <span className="blind">뒤로</span>
+                </i>
+              </Link>
+              <div className="btn-area flex-center">
+                {/* <a href="javascript:void(0);" className="btn share">
+              <i className="ico share">
+                <span>공유하기</span>
               </i>
-            </Link>
-            <div className="btn-area flex-center">
-              {/* <a href="javascript:void(0);" className="btn share">
-						<i className="ico share">
-							<span>공유하기</span>
-						</i>
-					</a> */}
-            </div>
-          </header>
+            </a> */}
+              </div>
+            </header>
 
-          <div id="content" className="story detail fade-in">
-            <div id="storyBanner" className="swiper-container">
-              <ul className="swiper-wrapper data-list" slot="container-start">
-                <li className="swiper-slide">
-                  <div className="banner-wrap">
-                    <div className="img-wrap">
-                      <img src={axioData?.contentData?.data?.thumb} alt="스토리 이미지" />
+            <div id="content" className="story detail fade-in">
+              <div id="storyBanner" className="swiper-container">
+                <ul className="swiper-wrapper data-list" slot="container-start">
+                  <li className="swiper-slide">
+                    <div className="banner-wrap">
+                      <div className="img-wrap">
+                        <img src={axioData?.contentData?.data?.thumb} alt="스토리 이미지" />
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              {/* <div className="swiper-pagination"></div> */}
+
+              <div className="story-detail-wrap">
+                <section className="section">
+                  <div className="item dalkomm-story">
+                    <div className="badge-wrap">
+                      <span className="badge square new">NEW</span>
+                      {/* 달콤스토리 .badge.square 타입
+                                      .badge.square.story : 브랜드 스토리 콘텐츠
+                                      .badge.square.event : 이벤트/프로모션
+                                      .badge.square.store : 신규 매장 소개
+                                      .badge.square.new   : 신메뉴 소개 
+                                      .badge.square.pick  : 달콤 굿즈 소개
+                                  */}
+                    </div>
+                    <div className="data-wrap">
+                      <h2 className="title">{axioData?.contentData?.data?.title}</h2>
+                      <p className="text">{axioData?.contentData?.data?.sub_title}</p>
+                      <p className="date">{axioData?.contentData?.data?.date}</p>
                     </div>
                   </div>
-                </li>
-              </ul>
-            </div>
-            {/* <div className="swiper-pagination"></div> */}
-
-            <div className="story-detail-wrap">
-              <section className="section">
-                <div className="item dalkomm-story">
-                  <div className="badge-wrap">
-                    <span className="badge square new">NEW</span>
-                    {/* 달콤스토리 .badge.square 타입
-                                    .badge.square.story : 브랜드 스토리 콘텐츠
-                                    .badge.square.event : 이벤트/프로모션
-                                    .badge.square.store : 신규 매장 소개
-                                    .badge.square.new   : 신메뉴 소개 
-                                    .badge.square.pick  : 달콤 굿즈 소개
-                                */}
+                  <div className="item board">
+                    <div dangerouslySetInnerHTML={{ __html: axioData?.contentDetail }}></div>
                   </div>
-                  <div className="data-wrap">
-                    <h2 className="title">{axioData?.contentData?.data?.title}</h2>
-                    <p className="text">{axioData?.contentData?.data?.sub_title}</p>
-                    <p className="date">{axioData?.contentData?.data?.date}</p>
+                </section>
+
+                <section className="section">
+                  <div className="w-inner">
+                    <h3 className="section-title">
+                      <span>이벤트 메뉴 바로가기</span>
+                    </h3>
+                    <ul className="data-list">
+                      {axioData?.contentData?.menu?.map((e, i) => (
+                        <li key={i}>
+                          <Link to={`/order/infoDetail/${e?.code}`} className="item order">
+                            <div className="img-wrap">
+                              <img src={e?.img} alt="청포도 블렌디드" />
+                            </div>
+                            <div className="detail-wrap">
+                              <div className="order-info">
+                                <p className="title">
+                                  {e?.name}
+                                  <span className="en">{e?.name_eng}</span>
+                                </p>
+                              </div>
+                              <div className="price-wrap">
+                                <p className="price">{Number(e?.price).toLocaleString("ko-KR")}원</p>
+                              </div>
+                            </div>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </div>
-                <div className="item board">
-                  <div dangerouslySetInnerHTML={{ __html: axioData?.contentDetail }}></div>
-                </div>
-              </section>
-
-              <section className="section">
-                <div className="w-inner">
-                  <h3 className="section-title">
-                    <span>이벤트 메뉴 바로가기</span>
-                  </h3>
-                  <ul className="data-list">
-                    {axioData?.contentData?.menu?.map((e, i) => (
-                      <li key={i}>
-                        <Link to={`/order/infoDetail/${e?.code}`} className="item order">
-                          <div className="img-wrap">
-                            <img src={e?.img} alt="청포도 블렌디드" />
-                          </div>
-                          <div className="detail-wrap">
-                            <div className="order-info">
-                              <p className="title">
-                                {e?.name}
-                                <span className="en">{e?.name_eng}</span>
-                              </p>
-                            </div>
-                            <div className="price-wrap">
-                              <p className="price">{Number(e?.price).toLocaleString("ko-KR")}원</p>
-                            </div>
-                          </div>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </section>
-            </div>
-
-            {/* 목록으로 버튼 영역 */}
-            <div className="fixed-con active">
-              <div className="btn-area">
-                {axioData?.contentData?.prev && (
-                  <a onClick={() => handleLocation(axioData?.contentData?.prev)} className="btn page prev">
-                    <span className="blind">이전</span>
-                  </a>
-                )}
-                <Link to="/story/list" className="btn full x-large bdr">
-                  <strong>목록으로</strong>
-                </Link>
-                {axioData?.contentData?.next && (
-                  <a onClick={() => handleLocation(axioData?.contentData?.next)} className="btn page next">
-                    <span className="blind">다음</span>
-                  </a>
-                )}
+                </section>
               </div>
-            </div>
-            {/* // 목록으로 버튼 영역 */}
 
-            <button type="button" id="moveScrollTop" className="btn scroll-top" onClick={() => moveScrollTop()}>
-              <i className="ico arr-top"></i>
-            </button>
+              {/* 목록으로 버튼 영역 */}
+              <div className="fixed-con active">
+                <div className="btn-area">
+                  {axioData?.contentData?.prev && (
+                    <a onClick={() => handleLocation(axioData?.contentData?.prev)} className="btn page prev">
+                      <span className="blind">이전</span>
+                    </a>
+                  )}
+                  <Link to="/story/list" className="btn full x-large bdr">
+                    <strong>목록으로</strong>
+                  </Link>
+                  {axioData?.contentData?.next && (
+                    <a onClick={() => handleLocation(axioData?.contentData?.next)} className="btn page next">
+                      <span className="blind">다음</span>
+                    </a>
+                  )}
+                </div>
+              </div>
+              {/* // 목록으로 버튼 영역 */}
+
+              <button type="button" id="moveScrollTop" className="btn scroll-top" onClick={() => moveScrollTop()}>
+                <i className="ico arr-top"></i>
+              </button>
+            </div>
+            {/* // #content */}
           </div>
-          {/* // #content */}
+          {/* // #container */}
         </div>
-        {/* // #container */}
+        {/* // #wrap */}
+      </React.Fragment>
+    );
+  } else {
+    <div id="wrap" className="wrap">
+      <div id="container" className="container">
+        <header id="header" className="header bg-transparent">
+          <h1 className="page-title">
+            <span className="blind">달콤스토리</span>
+          </h1>
+          <Link type="button" className="btn back" to={"/story/list"}>
+            <i className="ico back">
+              <span className="blind">뒤로</span>
+            </i>
+          </Link>
+          <div className="btn-area flex-center">
+            {/* <a href="javascript:void(0);" className="btn share">
+              <i className="ico share">
+                <span>공유하기</span>
+              </i>
+            </a> */}
+          </div>
+        </header>
       </div>
-      {/* // #wrap */}
-    </React.Fragment>
-  );
+    </div>;
+  }
 }
--- src/Pages/Story/StoryList.js
+++ src/Pages/Story/StoryList.js
@@ -27,71 +27,86 @@ export default function StoryList() {
   useEffect(() => {
     fadeOut();
   }, [axioData]);
-  return (
-    <React.Fragment>
-      <GoContents />
+  if (axioData) {
+    return (
+      <React.Fragment>
+        <GoContents />
 
-      <div id="wrap" className="wrap">
-        <div id="container" className="container">
-          <header id="header" className="header">
-            <h1 className="page-title">달콤 스토리</h1>
-            <Link type="button" className="btn back" to={"/menu"}>
-              <i className="ico back">
-                <span className="blind">뒤로</span>
-              </i>
-            </Link>
-          </header>
+        <div id="wrap" className="wrap">
+          <div id="container" className="container">
+            <header id="header" className="header">
+              <h1 className="page-title">달콤 스토리</h1>
+              <Link type="button" className="btn back" to={"/menu"}>
+                <i className="ico back">
+                  <span className="blind">뒤로</span>
+                </i>
+              </Link>
+            </header>
 
-          <div id="content" className="story fade-in">
-            {/* 주문하기 */}
-            <section className="section">
-              <div className="w-inner">
-                <ul className="data-list">
-                  {axioData?.contentList?.map((e, i) => (
-                    <li key={i}>
-                      <Link to={`/story/detail/${e?.seq}`} className="item dalkomm-story">
-                        <div className="badge-wrap">
-                          {e?.cate === "STORY" ? (
-                            <span className="badge square story">STORY</span>
-                          ) : e?.cate === "EVENT" ? (
-                            <span className="badge square event">EVENT</span>
-                          ) : e?.cate === "NEW" ? (
-                            <span className="badge square new">NEW</span>
-                          ) : e?.cate === "PICK" ? (
-                            <span className="badge square pick">PICK</span>
-                          ) : (
-                            ""
-                          )}
+            <div id="content" className="story fade-in">
+              {/* 주문하기 */}
+              <section className="section">
+                <div className="w-inner">
+                  <ul className="data-list">
+                    {axioData?.contentList?.map((e, i) => (
+                      <li key={i}>
+                        <Link to={`/story/detail/${e?.seq}`} className="item dalkomm-story">
+                          <div className="badge-wrap">
+                            {e?.cate === "STORY" ? (
+                              <span className="badge square story">STORY</span>
+                            ) : e?.cate === "EVENT" ? (
+                              <span className="badge square event">EVENT</span>
+                            ) : e?.cate === "NEW" ? (
+                              <span className="badge square new">NEW</span>
+                            ) : e?.cate === "PICK" ? (
+                              <span className="badge square pick">PICK</span>
+                            ) : (
+                              ""
+                            )}
 
-                          {/* 달콤스토리 .badge.square 타입
-                                            .badge.square.story : 브랜드 스토리 콘텐츠
-                                            .badge.square.event : 이벤트/프로모션
-                                            .badge.square.store : 신규 매장 소개
-                                            .badge.square.new   : 신메뉴 소개 
-                                            .badge.square.pick  : 달콤 굿즈 소개
-                                        */}
-                        </div>
-                        <div className="img-wrap">
-                          <img src={e?.thumb} alt="{title}" />
-                        </div>
-                        <div className="data-wrap">
-                          <h2 className="title">{e?.title}</h2>
-                          <p className="text">{e?.sub_title}</p>
-                          <p className="date">{e?.date}</p>
-                        </div>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </section>
-            {/* // 주문하기 */}
+                            {/* 달콤스토리 .badge.square 타입
+                                              .badge.square.story : 브랜드 스토리 콘텐츠
+                                              .badge.square.event : 이벤트/프로모션
+                                              .badge.square.store : 신규 매장 소개
+                                              .badge.square.new   : 신메뉴 소개 
+                                              .badge.square.pick  : 달콤 굿즈 소개
+                                          */}
+                          </div>
+                          <div className="img-wrap">
+                            <img src={e?.thumb} alt="{title}" />
+                          </div>
+                          <div className="data-wrap">
+                            <h2 className="title">{e?.title}</h2>
+                            <p className="text">{e?.sub_title}</p>
+                            <p className="date">{e?.date}</p>
+                          </div>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </section>
+              {/* // 주문하기 */}
+            </div>
+            {/* // #content */}
           </div>
-          {/* // #content */}
+          {/* // #container */}
         </div>
-        {/* // #container */}
+        {/* // #wrap */}
+      </React.Fragment>
+    );
+  } else {
+    <div id="wrap" className="wrap">
+      <div id="container" className="container">
+        <header id="header" className="header">
+          <h1 className="page-title">달콤 스토리</h1>
+          <Link type="button" className="btn back" to={"/menu"}>
+            <i className="ico back">
+              <span className="blind">뒤로</span>
+            </i>
+          </Link>
+        </header>
       </div>
-      {/* // #wrap */}
-    </React.Fragment>
-  );
+    </div>;
+  }
 }
