b0fe7d06b04cbb8ba393c82fffed07f4d2b07c9c
kimjunyeob <kimjun@hanbom.com>
2022-01-07 19:00:01 +0900

    고객센터 ui 업데이트

--- src/Pages/DevTest.js
+++ src/Pages/DevTest.js
@@ -7,15 +7,17 @@ export default function DevTest() {
 
   return (
     <div style={{ wordBreak: "break-all" }}>
-      <p> X-DALKOMM-AUTH-TOKEN : {getCookieValue("X-DALKOMM-AUTH-TOKEN")}</p>
+      <p> 개발자 : 김준엽</p>
+      <br />
+      <p> 기획자 : 예원숭이</p>
+      <br />
+      <p> 사용하시기에 편하시나요?</p>
       <br />
       <p> Authorization : {getCookieValue("Authorization")}</p>
       <br />
       <p> X-DALKOMM-ACCESS-TOKEN : {getCookieValue("X-DALKOMM-ACCESS-TOKEN")}</p>
       <br />
       <p> X-DALKOMM-APP-VERSION : {getCookieValue("X-DALKOMM-APP-VERSION")}</p>
-      <br />
-      <p> auth : {state?.auth}</p>
       <br />
       <p> udid : {getCookieValue("udid")}</p>
       <br />
